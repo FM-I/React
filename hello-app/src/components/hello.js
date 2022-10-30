63aa1bdb24c54c50a251072a31bef28733f480a0
Max <maxfilimonchyk@gmail.com>
2022-10-30 14:23:57 +0200

    lab1

---
+++ hello-app/src/components/hello.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+export function Hello(){
+    const name = "Maxim";
+    return (
+        <h1>Hello {name}</h1>
+    )
+}
---
+++ hello-app/src/components/table.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+export function ShowTable() {
+    return (
+      <table className="MyTable">
+        <tr>
+          <td><b>First name</b></td>
+          <td>Filimonchuk</td>
+        </tr>
+        <tr>
+          <td><b>Last Name</b></td>
+          <td>Maxim</td>
+        </tr>
+        <tr>
+          <td><b>Occupation</b></td>
+          <td>1c dev</td>
+        </tr>
+      </table>
+    );
+  }
